ff895902dddb99364ba4292f1e4f9206357d56bc
emr4706 <emr4706@gmail.com>
2023-11-06 10:31:19 +0100

    Bank account testing is done

--- src/test/java/Bank_Accounts.java
+++ src/test/java/Bank_Accounts.java
@@ -1,8 +1,108 @@
+import Model.BankAccount;
+import Model.Login;
+import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
 
-public class Bank_Accounts {
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class Bank_Accounts extends Setup_Login {
+
+    BankAccount bankAccount = new BankAccount();
+    String bankAccountId="";
+    Faker faker = new Faker();
     @Test
     public void createBankAccount() {
-        //TODO
+        bankAccount.name = faker.name().name();
+        bankAccount.iban = faker.idNumber().invalid();
+        bankAccount.currency = "EUR";
+        bankAccount.active = true;
+        bankAccount.integrationCode = faker.code().imei();
+        bankAccount.schoolId = "646cbb07acf2ee0d37c6d984";
+
+        bankAccountId =
+        given()
+                .spec(reqSpec)
+                .body(bankAccount)
+
+                .when()
+                .post("school-service/api/bank-accounts")
+
+                .then()
+                .log().body()
+                .statusCode(201)
+                .extract().path("id")
+
+        ;
     }
+
+    @Test(dependsOnMethods = "createBankAccount")
+    public void createBankAccountNegative() {
+
+                given()
+                        .spec(reqSpec)
+                        .body(bankAccount)
+
+                        .when()
+                        .post("school-service/api/bank-accounts")
+
+                        .then()
+                        .log().body()
+                        .statusCode(400)
+        ;
+    }
+
+
+    @Test(dependsOnMethods = "createBankAccountNegative")
+    public void updateBankAccount() {
+        bankAccount.id = bankAccountId;
+        bankAccount.name = bankAccount.name + " updated";
+
+                given()
+                        .spec(reqSpec)
+                        .body(bankAccount)
+
+                        .when()
+                        .put("school-service/api/bank-accounts")
+
+                        .then()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().path("id")
+
+        ;
+    }
+
+    @Test(dependsOnMethods = "updateBankAccount")
+    public void deleteBankAccount() {
+
+        given()
+                .spec(reqSpec)
+                .body(bankAccount)
+
+                .when()
+                .delete("school-service/api/bank-accounts/" + bankAccountId)
+
+                .then()
+                .log().body()
+                .statusCode(200)
+        ;
+    }
+
+    @Test(dependsOnMethods = "updateBankAccount")
+    public void deleteBankAccountNegative() {
+
+        given()
+                .spec(reqSpec)
+                .body(bankAccount)
+
+                .when()
+                .delete("school-service/api/bank-accounts/" + bankAccountId)
+
+                .then()
+                .log().body()
+                .statusCode(400)
+        ;
+    }
+
 }
